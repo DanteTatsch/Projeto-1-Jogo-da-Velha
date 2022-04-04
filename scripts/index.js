532f0be349df13937270d5f853534d509b31dad3
dante.tatsch@hotmail.com <dante.tatsch@gmail.com>
2022-04-04 18:01:12 +0100

    ainda não inseri os codigos js

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,18 @@
-const casas = document.getElementsByTagName('input');
-const b_reiniciar = document.getElementById('reiniciar');
-const label_jogador = document.getElementById('jogador');
+const jog1 =  document.getElementById("player1");
+const jog2 = document.getElementById("player2")
 
 
-let jogador = "_"
-let vencedor = "_"
+/** sortear jogador */
+
+
+b_iniciar.addEventListener('click', function getRandomInt(jog1, jog2) {
+    let jog1 = 0;
+    let jog2 = 1;
+    return Math.floor(Math.random() * 2 + jog1;
+  })
+
+    
+
+
+
+       
