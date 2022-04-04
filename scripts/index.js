73604894c9d2aa3d44865c4f9eb4054e420478fe
dante.tatsch@hotmail.com <dante.tatsch@gmail.com>
2022-04-04 11:12:15 +0100

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,7 @@
+const casas = document.getElementsByTagName('input');
+const b_reiniciar = document.getElementById('reiniciar');
+const label_jogador = document.getElementById('jogador');
+
+
+let jogador = "_"
+let vencedor = "_"
