e67b8c0b557f2853ae58833cb1a1697d4d2d7aba
dante.tatsch@hotmail.com <dante.tatsch@gmail.com>
2022-04-06 16:06:57 +0100

    commit final

--- scripts/index.js
+++ scripts/index.js
@@ -1,38 +1,88 @@
-var jogador, vencedor = null;
-var jogadorSelecionado = document.getElementById('jogador-selecionado');
-var vencedorSelecionado = document.getElementById('vencedor-selecionado');
+const statusDisplay = document.querySelector('.game--status');
 
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "", ""];
 
+const winningMessage = () => `Jogador ${currentPlayer} venceu!`;
+const drawMessage = () => `Velha!`;
+const currentPlayerTurn = () => `Vez do jogador ${currentPlayer}'s`;
 
-mudarJogador( 'X' );
+statusDisplay.innerHTML = currentPlayerTurn();
 
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 
-function escolherQuadrado(id) {
-    if (vencedor !== null) {
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningConditions[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break
+        }
+    }
+
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
         return;
     }
 
-    var quadrado = document.getElementById(id);
-    if (quadrado.innerHTML !== '-') {
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
         return;
     }
-    
 
-    quadrado.innerHTML = jogador;
-    quadrado.style.color='#000';
+    handlePlayerChange();
+}
 
-    if (jogador === 'X') {
-        jogador = 'O';
-    } else {
-        jogador = 'X';
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-cell-index'));
+
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
     }
 
-    mudarJogador(jogador);
-    checaVencedor();
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
 }
 
-function mudarJogador(valor) {
-    jogador = valor;
-    jogadorSelecionado.innerHTML = jogador;
+function handleRestartGame() {
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""];
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
 }
 
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('.game--restart').addEventListener('click', handleRestartGame);
